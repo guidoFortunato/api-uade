8de4b8d254362180d7abe17e9c7f645fb2be94f9
Guido <46787485+guidoFortunato@users.noreply.github.com>
2024-04-09 13:33:33 -0300

    fondo en auth y home

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export const AuthLayout = () => {
   return (
-    <main className="flex justify-center items-center min-h-screen px-4 sm:px-0">
+    <main className="flex justify-center items-center min-h-screen px-4 sm:px-0 background-auth">
       {/* <div className="w-full sm:w-[500px] px-10"> */}
       <div className="w-full sm:w-[600px]">
         <Outlet />
