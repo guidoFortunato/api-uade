51b905ac3faffe6e8874243a68c372a46fc43c21
Vicky <vickyendendyk@hotmail.com>
2024-04-22 21:35:14 -0300

    MejorasFotter

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    //chequear que funcione el color del background
-    <footer className="rounded-lg shadow m-4  bg-#3C096C"> 
+    //chequear que funcione el color del background 
+    <footer className="rounded-lg shadow m-4 bg-[#993fecdc]"> 
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:flex-col md:items-center font-normal md:justify-center">
-      <p className="text-sm text-gray-500 sm:text-center font-semibold italic sm:text-6x1 ">FrameLand</p>
-        <span className="text-sm text-gray-500 font-medium sm:text-center ">
+      <p className="text-sm text-white sm:text-center font-semibold italic sm:text-6x1 ">FrameLand</p>
+        <span className="text-sm text-white font-medium sm:text-center ">
         <p>Un espacio con especial atención al cliente latinoamericano </p>
           <Link to="/" className=" ml-1 text-xs">
           © { new Date().getFullYear() } - Todos los derechos reservado
